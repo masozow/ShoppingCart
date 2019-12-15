651e7cc9dd3f2512fee073b9c2ede5d00a0dba54
masozow <masozow@gmail.com>
2019-12-14 22:53:47 -0600

    Advances on JS

--- JS/site.js
+++ JS/site.js
@@ -6,5 +6,22 @@ else{
 }
 
 function ready(){
-    
+    const items = document.querySelectorAll('.content-item');
+    const _shoppingCart = document.querySelector('#shopping_cart');
+    let _productsCounter = Number.parseInt(_shoppingCart.dataset.productsCount);
+    console.log(_productsCounter);
+    items.forEach((item)=>{
+        let shoppingCart = item.querySelector('.shoppingCart');
+        shoppingCart.addEventListener('click',(e)=>{
+            e.preventDefault();
+            _productsCounter+=1;
+            console.log(e.target);
+            _shoppingCart.dataset.productsCount=_productsCounter;
+        });
+        console.log(shoppingCart);
+    });
+}
+function changeProductsCount(counter){
+    this._counter=counter<0?1:counter;
+    _shoppingCart.dataset.productsCount=_counter;
 }
